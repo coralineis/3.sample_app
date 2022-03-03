987c0088f24db651332de2ab1db282e4bcfe4216
EC2 Default User <ec2-user@ip-172-31-38-45.ec2.internal>
2022-03-03 02:29:24 +0000

    [Finish] 3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,13 +5,14 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
   def index
     @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
